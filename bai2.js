5fc5d165fc010a8bb069cc00675ddae29062330a
BBMinhh <136958412+BBMinhh@users.noreply.github.com>
2023-08-06 03:22:44 +0700

    Add files via upload

---
+++ bai2.js
@@ -0,0 +1,16 @@
+function A(){
+    a = prompt('Nhap A')
+    }
+    function B(){
+    b = prompt('Nhap B')
+    }
+    function KQ() {
+      
+        x = Math.sqrt((a * a) + (b * b));
+        alert(x)
+    }
+    function KQ1(){
+        z = x/2;
+        alert(z)
+    }
+    
